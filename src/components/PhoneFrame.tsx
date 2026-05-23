f35806cf435420d25fc3fd8d7de207349d9a24d3
Lucas Nunes <lucasnunes@MacBook-Pro-de-Lucas.local>
2026-05-23 19:26:40 -0300

    refactor: PhoneFrame now accepts children instead of image src

--- src/components/PhoneFrame.tsx
+++ src/components/PhoneFrame.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
+import type { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 
 type Props = {
-  src: string;
-  alt: string;
+  children: ReactNode;
   className?: string;
 };
 
-export function PhoneFrame({ src, alt, className }: Props) {
+export function PhoneFrame({ children, className }: Props) {
   return (
     <div
       className={cn(
@@ -19,16 +18,10 @@ export function PhoneFrame({ src, alt, className }: Props) {
       <div className="pointer-events-none absolute -left-[3px] top-40 h-16 w-[3px] rounded-l-sm bg-slate-700" />
       <div className="pointer-events-none absolute -right-[3px] top-32 h-20 w-[3px] rounded-r-sm bg-slate-700" />
 
-      <div className="absolute left-1/2 top-2 z-10 h-7 w-32 -translate-x-1/2 rounded-full bg-slate-900" />
+      <div className="absolute left-1/2 top-2 z-20 h-7 w-32 -translate-x-1/2 rounded-full bg-slate-900" />
 
-      <div className="relative h-full w-full overflow-hidden rounded-[2rem] bg-white">
-        <Image
-          src={src}
-          alt={alt}
-          fill
-          sizes="(min-width: 1024px) 300px, 260px"
-          className="object-cover object-top"
-        />
+      <div className="relative h-full w-full overflow-hidden rounded-[2rem] bg-[#F8FAFC]">
+        {children}
       </div>
     </div>
   );
